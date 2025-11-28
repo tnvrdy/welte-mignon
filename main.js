3fd794a47baf332b64c1191cf52cebba92183605
tnvrdy <tanvired@stanford.edu>
2025-11-28 14:21:20 -0600

    quick play button

--- main.js
+++ main.js
@@ -2,7 +2,14 @@
  * File: main.js
  * -------------
  */
+const MIDDLE_C = new Audio("middleC.mp3");
 
 function main() {
+    let playButton = document.getElementById("play-button");
     
+    let playClickAction = function() {
+        MIDDLE_C.play();
+    }
+
+    playButton.addEventListener("click", playClickAction);
 }
